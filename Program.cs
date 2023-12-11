32b7367a7b6dd5ca226c7afc1e4cb4d822dbb56e
Suraj Rajendra Pardeshi <152958796+SurajPardeshi636@users.noreply.github.com>
2023-12-11 14:55:53 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 2;
+
+            switch(i)
+                {
+
+                case 1:
+                    Console.WriteLine("one");
+                    break;
+
+                    case 2:
+                    Console.WriteLine("two");
+                    break;
+
+                    case 3:
+                    Console.WriteLine("three");
+                    break;
+
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
